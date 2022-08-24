19e49dca6327611e64bf3f6acc8b1514a9159961
Warren Lee <wplee327@users.noreply.github.com>
2022-08-24 15:08:37 -0400

    Add testing for PasswordFactory; Adjust how ConnectionFactory and PropertiesFactory are used in tests

--- rest/src/test/java/com/revature/test/UtilsTest.java
+++ rest/src/test/java/com/revature/test/UtilsTest.java
@@ -6,19 +6,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.revature.rest.utils.ConnectionFactory;
+import com.revature.rest.utils.PasswordFactory;
 import com.revature.rest.utils.PropertiesFactory;
 
 public class UtilsTest {
-	ConnectionFactory connectionFactory = ConnectionFactory.getInstance();
-	PropertiesFactory propertiesFactory = PropertiesFactory.getInstance();
-	@Test
-	public void testProperties() {
-		Properties properties = propertiesFactory.loadProperties();
-		Assert.assertFalse(properties.isEmpty());
-		Assert.assertEquals(properties.getProperty("db_url"), "jdbc:postgresql://demodb.ci6atgv104kr.us-east-1.rds.amazonaws.com:5432/wyns_wild_wares");
-	}
 	@Test
 	public void testConnection() {
-		Assert.assertNotNull(connectionFactory.getConnection());
+		Assert.assertNotNull(ConnectionFactory.getInstance().getConnection());
+	}
+	@Test
+	public void testPassword() {
+		String testPassword = "password123";
+		String encodedTestPassword = PasswordFactory.getInstance().encodePassword(testPassword);
+		Assert.assertTrue(PasswordFactory.getInstance().matchPassword(testPassword, encodedTestPassword));
+	}
+	@Test
+	public void testProperties() {
+		Properties properties = PropertiesFactory.getInstance().loadProperties();
+		Assert.assertFalse(properties.isEmpty());
+		Assert.assertEquals(properties.getProperty("db_url"), "jdbc:postgresql://demodb.ci6atgv104kr.us-east-1.rds.amazonaws.com:5432/wyns_wild_wares");
 	}
 }
