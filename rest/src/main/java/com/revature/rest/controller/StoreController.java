debdb6febb7624b7db3844c1669267ea16b29a63
Warren Lee <wplee327@users.noreply.github.com>
2022-09-04 15:38:31 -0400

    Implement StoreService and StoreController to handle GET and POST requests; Update routing in web.xml

---
+++ rest/src/main/java/com/revature/rest/controller/StoreController.java
@@ -0,0 +1,86 @@
+package com.revature.rest.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.rest.dao.StoreDAO;
+import com.revature.rest.models.Store;
+import com.revature.rest.services.StoreService;
+
+/**
+ * Handles servlet requests for stores.
+ * @author Warren Lee
+ */
+public class StoreController extends HttpServlet {
+	private static Logger logger = LogManager.getLogger(StoreController.class);
+	private static StoreService storeService = new StoreService(new StoreDAO());
+	private static ObjectMapper objectMapper = new ObjectMapper();
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String requestURI = req.getRequestURI().replace("/rest/", "");
+		Map<String, String[]> parameterMap = req.getParameterMap();
+		resp.setContentType("application/json");
+		String jsonResponse = null;
+		switch (requestURI) {
+		case "store":
+			Store store = new Store();
+			store.setName("Store not found");
+			if (parameterMap.containsKey("id")) {
+				int id = 0;
+				try {
+					id = Integer.parseInt(parameterMap.get("id")[0]);
+					if (id > 0) {
+						store = storeService.getStoreById(id);
+					}
+				} catch (NumberFormatException e) {
+					logger.warn("Invalid id parameter.", e);
+				}
+			}
+			jsonResponse = objectMapper.writeValueAsString(store);
+			resp.getWriter().println(jsonResponse);
+			break;
+		case "stores":
+			List<Store> stores = null;
+			if (parameterMap.isEmpty()) stores = storeService.getAllStores();
+			jsonResponse = objectMapper.writeValueAsString(stores);
+			resp.getWriter().println(jsonResponse);
+			break;
+		default:
+			super.doGet(req, resp);
+			break;
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String requestURI = req.getRequestURI().replace("/rest/", "");
+		resp.setContentType("application/json");
+		String jsonResponse = null;
+		switch (requestURI) {
+		case "store/add":
+			String jsonRequest = req.getReader().lines().collect(Collectors.joining());
+			Store store = objectMapper.readValue(jsonRequest, Store.class);
+			store = storeService.addStore(store);
+			if (store.getId() < 1) {
+				store = new Store();
+				store.setName("Failed to add new store");
+			}
+			jsonResponse = objectMapper.writeValueAsString(store);
+			resp.getWriter().println(jsonResponse);
+			break;
+		default:
+			super.doPost(req, resp);
+			break;
+		}
+	}
+}
---
+++ rest/src/main/java/com/revature/rest/services/StoreService.java
@@ -0,0 +1,60 @@
+package com.revature.rest.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.revature.rest.dao.DAO;
+import com.revature.rest.models.Store;
+
+/**
+ * A service to perform custom logic on stores.
+ * @author Warren Lee
+ */
+public class StoreService {
+	private static Logger logger = LogManager.getLogger(StoreService.class);
+	private DAO<Store> storeDAO;
+	/**
+	 * Constructs a StoreService using the input DAO.
+	 * @param storeDAO the DAO instance
+	 */
+	public StoreService(DAO<Store> storeDAO) {
+		this.storeDAO = storeDAO;
+	}
+	/**
+	 * Adds a new store to the database and returns it with its id.
+	 * @param newStore the new store
+	 * @return the store with its id
+	 */
+	public Store addStore(Store newStore) {
+		logger.info("Adding new store...");
+		return storeDAO.addInstance(newStore);
+	}
+	/**
+	 * Returns a list of all stores.
+	 * @return a list of all stores
+	 */
+	public List<Store> getAllStores() {
+		logger.info("Getting all stores...");
+		return storeDAO.getAllInstances();
+	}
+	/**
+	 * Returns a store with matching id.
+	 * @param id the store's id
+	 * @return a store with matching id
+	 */
+	public Store getStoreById(int id) {
+		logger.info("Getting store with id = " + id + "...");
+		List<Store> stores = getAllStores();
+		Optional<Store> foundStore = stores.stream().filter(store -> store.getId() == id).findFirst();
+		if (foundStore.isPresent()) return foundStore.get();
+		else {
+			logger.warn("Failed to get store with id = " + id + ".");
+			Store notFoundStore = new Store();
+			notFoundStore.setName("Store not found");
+			return notFoundStore;
+		}
+	}
+}
